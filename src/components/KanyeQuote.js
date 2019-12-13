df73902fcca756f6b517c1bbcc2f89aea69976ee
Andrew Maddocks <andrewmadd@icloud.com>
2019-12-12 17:24:05 -0700

    mvp done

---
+++ src/components/KanyeQuote.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { connect } from "react-redux";
+import { getQuote } from "../actions";
+import { ClipLoader } from "react-spinners";
+
+const KanyeQuote = props => {
+  return (
+    <div className="kanye-div">
+      <div>
+        <h1>What Does Kanye Say?</h1>
+      </div>
+      <div>
+        {!props.quote && !props.isFetching && (
+          <p> Closed on sunday! You're my Chic-fil-a!!! </p>
+        )}
+        {props.isFetching && <ClipLoader color={"red"} timeout={4000} />}
+        {props.quote && <p>"{props.quote}"</p>}
+      </div>
+      <div>
+        <button onClick={props.getQuote}></button>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    quote: state.quote,
+    isFetching: state.isFetching,
+    error: state.error
+  };
+};
+
+export default connect(mapStateToProps, { getQuote })(KanyeQuote);
